1831eb4caaca25063f5330eefe34b614166d1563
Haofan Fang <haofanfang@gmail.com>
2020-04-20 21:33:03 +0800

    update Volley Request Test

--- app/src/main/java/com/supremesir/gallerydemo/MainActivity.java
+++ app/src/main/java/com/supremesir/gallerydemo/MainActivity.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.supremesir.gallerydemo.databinding.ActivityMainBinding;
+
+/**
+ * @author fang
+ */
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "https://www.jd.com";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(
+                StringRequest.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        binding.textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        binding.textView.setText("Error");
+                    }
+                }
+        );
+        queue.add(stringRequest);
     }
 }
